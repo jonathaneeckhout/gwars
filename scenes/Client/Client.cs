a6c42eb1ed67ab6cabaf49fefbbb415c453e16c0
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2024-03-25 12:01:32 +0100

    create commodities

--- scenes/Client/Client.cs
+++ scenes/Client/Client.cs
@@ -11,19 +11,7 @@ public partial class Client : Node
     public bool IsLoggedIn { get; set; } = false;
 
     // Called when the node enters the scene tree for the first time.
-    async public override void _Ready()
+    public override void _Ready()
     {
-
-        // if (!Multiplayer.IsServer())
-        // {
-        //     await ToSignal(GetTree(), "process_frame");
-        //     // Some entities take a bit to get added to the tree, do not update them until then.
-        //     if (!IsInsideTree())
-        //     {
-        //         await ToSignal(this, "tree_entered");
-        //     }
-
-        //     RpcId(1, "LoginToServer", "testuser", "testpassword");
-        // }
     }
 }
