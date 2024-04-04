d6a718847d4063c9b8328d15cb73870c79665300
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2024-04-04 15:37:18 +0200

    show trainingsprogress

--- scenes/units/buildings/Townhall/Townhall.cs
+++ scenes/units/buildings/Townhall/Townhall.cs
@@ -9,14 +9,18 @@ public partial class Townhall : Unit
     public override bool IsRepairable { get; set; } = true;
     public override bool IsStorage { get; set; } = true;
     public override bool IsTrainingCenter { get; set; } = true;
-
     public new static readonly Dictionary<string, Dictionary> Units = new Dictionary<string, Dictionary>
     {
-        { "Worker", new Dictionary { { "Gold", 0 }, {"Food", 100}, {"Time", 3.0}, { "Scene", GD.Load<PackedScene>("res://scenes/units/minions/Worker/Worker.tscn") } } }
+        { "Worker", new Dictionary { { "Gold", 0 }, {"Food", 100}, {"Time", 10.0}, { "Scene", GD.Load<PackedScene>("res://scenes/units/minions/Worker/Worker.tscn") } } }
     };
-    private Array<Dictionary> trainingQueue = new Array<Dictionary>();
+    [Export]
+    public Array<string> TrainingQueue = new Array<string>();
+    [Export]
+    public float TrainingProgress = 0.0f;
 
     private Timer progressTimer = null;
+
+    private ProgressBar progressBar = null;
 
     public override void _Ready()
     {
@@ -29,6 +33,25 @@ public partial class Townhall : Unit
         progressTimer.Autostart = false;
         progressTimer.Timeout += OnProgressTimerTimeout;
         AddChild(progressTimer);
+
+        progressBar = GetNode<ProgressBar>("ProgressBar");
+
+        if (Multiplayer.IsServer())
+        {
+            SetProcess(false);
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        if (TrainingQueue.Count > 0)
+        {
+            progressBar.Value = TrainingProgress / (float)Units[(string)TrainingQueue[0]]["Time"] * 100.0f;
+        }
+        else
+        {
+            progressBar.Value = 0.0f;
+        }
 
     }
 
@@ -48,10 +71,7 @@ public partial class Townhall : Unit
             {
                 if (player.RemoveMaterials((uint)Units[unitType]["Gold"], (uint)Units[unitType]["Food"]))
                 {
-                    Vector2 SpawnPosition = Position + new Vector2(0, 64);
-                    // TODO: add unit to training queue
-
-                    trainingQueue.Add(new Dictionary { { "UnitType", unitType }, { "Progress", 0.0 } });
+                    TrainingQueue.Add(unitType);
 
                     if (progressTimer.IsStopped())
                     {
@@ -68,21 +88,22 @@ public partial class Townhall : Unit
 
     private void OnProgressTimerTimeout()
     {
-        if (trainingQueue.Count > 0)
+        if (TrainingQueue.Count > 0)
         {
-            trainingQueue[0]["Progress"] = (float)trainingQueue[0]["Progress"] + progressTimer.WaitTime;
-            if ((float)trainingQueue[0]["Progress"] >= (float)Units[(string)trainingQueue[0]["UnitType"]]["Time"])
+            TrainingProgress += (float)progressTimer.WaitTime;
+            if (TrainingProgress >= (float)Units[(string)TrainingQueue[0]]["Time"])
             {
                 Player player = Map.GetPlayer(PlayerName);
                 if (player != null)
                 {
-                    Map.ServerCreateEntity(player, (string)trainingQueue[0]["UnitType"], Position + new Vector2(0, 64));
+                    Map.ServerCreateEntity(player, (string)TrainingQueue[0], Position + new Vector2(0, 64));
                 }
-                trainingQueue.RemoveAt(0);
+                TrainingQueue.RemoveAt(0);
+                TrainingProgress = 0.0f;
             }
         }
 
-        if (trainingQueue.Count == 0)
+        if (TrainingQueue.Count == 0)
         {
             progressTimer.Stop();
         }
