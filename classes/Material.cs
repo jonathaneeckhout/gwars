9d881526e937bdbc14b3568836488e7d51651428
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2024-04-08 16:23:43 +0200

    move common synchronizer to class code

--- classes/Material.cs
+++ classes/Material.cs
@@ -3,5 +3,24 @@ using System;
 
 public partial class Material : StaticBody2D
 {
-	public virtual string MaterialType { get; set; } = "";
+    public virtual string MaterialType { get; set; } = "";
+    protected MultiplayerSynchronizer synchronizer = null;
+    protected SceneReplicationConfig sceneReplicationConfig = null;
+    public Material()
+    {
+        sceneReplicationConfig = new SceneReplicationConfig();
+        AddReplicationProperty(".:position", true, SceneReplicationConfig.ReplicationMode.OnChange);
+
+        synchronizer = new MultiplayerSynchronizer();
+        synchronizer.Name = "Synchronizer";
+        synchronizer.DeltaInterval = 0.1f;
+        synchronizer.ReplicationConfig = sceneReplicationConfig;
+        AddChild(synchronizer);
+    }
+    public void AddReplicationProperty(string property, bool spawn = true, SceneReplicationConfig.ReplicationMode mode = SceneReplicationConfig.ReplicationMode.OnChange)
+    {
+        sceneReplicationConfig.AddProperty(property);
+        sceneReplicationConfig.PropertySetSpawn(property, spawn);
+        sceneReplicationConfig.PropertySetReplicationMode(property, mode);
+    }
 }
